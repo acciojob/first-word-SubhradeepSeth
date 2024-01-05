15ee527bcf55b320bb98f265238ac191b87de63b
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2024-01-05 16:02:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	if (s==="") return "";
+	let str="";
+	for (let i=0;i<s.length;i++) {
+		if(i===0 && s.charAt(i)===" ") continue;
+		str=str+s.charAt(i);
+		if(s.charAt(i)===" ") break;
+	}
+	return str;
 }
-
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
