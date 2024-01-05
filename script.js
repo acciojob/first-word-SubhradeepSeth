5de27c61b822c82999a1249e72c00b0a00a96ed7
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2024-01-05 16:10:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function firstWord(s) {
   // your code here
 	if (s==="") return "";
-	let str="";
-	for (let i=0;i<s.length;i++) {
-		if(i===0 && s.charAt(i)===" ") continue;
+	let str="",k=0;
+	for(let i=0;i<s.length;i++){
+		if(s.charAt(i)!=" "){
+			k=i;
+			break;
+		}
+	}
+	for (let i=k;i<s.length;i++) {
+		// if(i===0 && s.charAt(i)===" ") continue;
 		str=str+s.charAt(i);
 		if(s.charAt(i)===" ") break;
 	}
